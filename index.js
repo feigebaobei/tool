5fa84e4fdaf21a43625534d8649882376f87c588
feigebaobei <18515195415@163.com>
2019-07-30 15:46:28 +0800

    getBrowser

--- index.js
+++ index.js
@@ -205,11 +205,55 @@ let straight = (s, numRows) => {
     return res
   }
 }
-
-
-
-
-
+/**
+ * 得到浏览器种类
+ */
+let getBrowser = () => {
+    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+    var isOpera = userAgent.indexOf("Opera") > -1; //判断是否Opera浏览器
+    var isIE = userAgent.indexOf("compatible") > -1
+            && userAgent.indexOf("MSIE") > -1 && !isOpera; //判断是否IE浏览器
+    var isEdge = userAgent.indexOf("Edge") > -1; //判断是否IE的Edge浏览器
+    var isFF = userAgent.indexOf("Firefox") > -1; //判断是否Firefox浏览器
+    var isSafari = userAgent.indexOf("Safari") > -1
+            && userAgent.indexOf("Chrome") == -1; //判断是否Safari浏览器
+    var isChrome = userAgent.indexOf("Chrome") > -1
+            && userAgent.indexOf("Safari") > -1; //判断Chrome浏览器
+    if (isIE) {
+        var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+        reIE.test(userAgent);
+        var fIEVersion = parseFloat(RegExp["$1"]);
+        if (fIEVersion == 7) {
+            return "IE7";
+        } else if (fIEVersion == 8) {
+            return "IE8";
+        } else if (fIEVersion == 9) {
+            return "IE9";
+        } else if (fIEVersion == 10) {
+            return "IE10";
+        } else if (fIEVersion == 11) {
+            return "IE11";
+        } else {
+            return "0";
+        }//IE版本过低
+        return "IE";
+    }
+    if (isOpera) {
+        return "Opera";
+    }
+    if (isEdge) {
+        return "Edge";
+    }
+    if (isFF) {
+        return "FF";
+    }
+    if (isSafari) {
+        return "Safari";
+    }
+    if (isChrome) {
+        return "Chrome";
+    }
+}
 // 罗马数字 => 阿拉伯数字
 var intToRoman = function(num) {
     if (num < 1 || num > 3999) {
@@ -224,18 +268,6 @@ var intToRoman = function(num) {
     }
     return res
 };
-r = intToRoman(58)
-// 得到无序数组的最长递增子序列
-var ascendSubArr = arr => {
-  let res = arr.slice(0, 1), i = 1
-  while (i < arr.length) {
-    if ((res[res.length - 2] || arr[i] - 1) < arr[i] && arr[i] < res[res.length - 1]) {
-      res[res.length - 1] = arr[i]
-    }
-    i++
-  }
-  return res
-}
 // 阿拉伯数字 => 罗马数字
 let romanToInt = s => {
   if (s < 0 || 3999 < s) {
@@ -262,3 +294,17 @@ let romanToInt = s => {
   }
   return res
 }
+// 得到无序数组的最长递增子序列
+var ascendSubArr = arr => {
+  let res = arr.slice(0, 1), i = 1
+  while (i < arr.length) {
+    if ((res[res.length - 2] || arr[i] - 1) < arr[i] && arr[i] < res[res.length - 1]) {
+      res[res.length - 1] = arr[i]
+    }
+    i++
+  }
+  return res
+}
+
+
+
