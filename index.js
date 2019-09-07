1c190a03b127d1c50f458539fe716edb75284dec
feigebaobei <18515195415@163.com>
2019-09-07 21:36:27 +0800

    栈 队列

--- index.js
+++ index.js
@@ -264,6 +264,86 @@ let romanToInt = s => {
 }
 
 /*
+栈
+*/
+let Stack = (function () {
+let items = new WeakMap()
+class Stack {
+  constructor () {
+    // this.items = []
+    // this.length = this.items.length
+    items.set(this, [])
+  }
+  // 得到数组
+  getArray () {
+    return items.get(this)
+  }
+  // 添加一个或多个元素
+  push (...args) {
+    // this.items.push(...args)
+    // items.get(this).push(...args)
+    this.getArray().push(...args)
+  }
+  // 弹出一个元素
+  pop () {
+    // return this.items.pop()
+    return this.getArray().pop()
+  }
+  // 返回栈顶的元素
+  peek () {
+    return this.getArray()[this.size() - 1]
+  }
+  isEmpty () {
+    return this.size() === 0
+  }
+  clear () {
+    items.set(this, [])
+  }
+  size () {
+    return items.get(this).length
+  }
+}
+return Stack
+})()
+/*
+队列
+*/
+let Queue = (function () {
+  let items = new WeakMap()
+  return class Queue {
+    constructor () {
+      items.set(this, [])
+    }
+    //
+    getArray () {
+      return items.get(this)
+    }
+    // 追加元素
+    enqueue (...args) {
+      this.getArray().push(...args)
+    }
+    // 移除队列的第一个元素，并返回移除的元素
+    dequeue () {
+      return this.getArray().shift()
+    }
+    front () {
+      return this.getArray()[0]
+    }
+    end () {
+      return this.getArray()[this.size() - 1]
+    }
+    isEmpty () {
+      return this.size() === 0
+    }
+    clear () {
+      items.set(this, [])
+    }
+    size () {
+      return items.get(this).length
+    }
+  }
+})()
+/*
 链表
 */
 class LinkBase {
--- run.js
+++ run.js
@@ -1,233 +1,35 @@
-/*
-双向链表
-*/
-class DoublyLinkedList {
+
+class Stack {
   constructor () {
-    this.head = null
-    this.length = 0
-    this.tail = null
+    this.items = []
+    // this.length = this.items.length
   }
-  static node (element) {
-    return {
-      element: element,
-      prev: null,
-      next: null
-    }
+  // 添加一个或多个元素
+  push (...arg) {
+    this.items.push(...arg)
   }
-  // 追加
-  append (element) {
-    let node = DoublyLinkedList.node(element)
-    if (this.length) {
-      let t = this.tail
-      t.next = node
-      node.prev = t
-      this.tail = node
-    } else {
-      this.head = node
-      this.tail = node
-    }
-    this.length++
+  // 弹出一个元素
+  pop () {
+    this.items.pop()
   }
-  // 在指定位置插入
-  insert (position, element) {
-    if (position > 0 && position <= this.length) {
-      let node = DoublyLinkedList.node(element), current, previous, index = 0
-      if (position === 0) { // head
-        // node.next = this.head
-        // this.head = node
-        if (this.length) {
-          node.next = this.head
-          this.head.prev = node
-          this.head = node
-        } else {
-          this.head = node
-          this.tail = node
-        }
-      } else {
-        if (position === this.length) { // tail
-          this.tail.next = node
-          node.prev = this.tail
-          this.tail = node
-        } else { // middle
-          current = this.head
-          while(index++ < position) {
-            previous = current
-            current = current.next
-          }
-          node.next = current
-          node.prev = previous
-          current.prev = node
-          previous.next = node
-        }
-      }
-      this.length++
-      return true
-    } else {
-      return false
-    }
+  // 返回栈顶的元素
+  peek () {
+    return this.items[this.length - 1]
   }
-  // 删除指定位置的节点，若删除成功则返回节点的element。否则返回false
-  removeAt (position) {
-    if (position > -1 && position < this.length) {
-      let current = this.head, previous, index = 0
-      if (position === 0) { // head
-        if (this.length === 1) {
-          this.head = null
-          this.tail = null
-        } else {
-          this.head = this.head.next
-          this.head.prev = null
-        }
-      } else {
-        if (position === this.length - 1) { // tail
-          // if (this.length) {}
-          current = this.tail
-          this.tail = current.prev
-          this.tail.next = null
-        } else { // middle
-          while (index++ < position) {
-            previous = current
-            current = current.next
-          }
-          previous.next = current.next
-          current.next.prev = previous
-        }
-      }
-      this.length--
-      return current.element
-    } else {
-      return false
-    }
+  isEmpty () {
+    return this.size() === 0
   }
-  // 删除指定元素
-  removeElement (element, all = false) {
-    let current = this.head, res = [], index = 0, previous, next
-    while (current) {
-      if (current.element === element) {
-        // previous = current.prev || this.head
-        // next = current.next || this.tail
-        // previous.next = next
-        // next()
-
-
-        // if (!current.prev && current.next) {
-        //   this.head = current.next
-        //   current.next.prev = null
-        // } else {
-        //   if (!current.prev && !current.next) {
-        //     this.head = null
-        //     this.tail = null
-        //   } else {
-
-        //   }
-        // }
-
-
-        if (!current.prev) { // 前面无元素
-          this.head = current.next
-          if (!current.next) { // 后面无元素
-            this.tail = current.prev
-          } else { // 后面有元素
-            current.next.prev = null
-          }
-        } else { // 前面有元素
-          if (!current.next) { // tail
-            this.tail = current.prev
-            // current.next = null
-          } else {
-            current.prev.next = current.next
-            current.next.prev = current.prev
-          }
-        }
-        this.length--
-        if (!all) {
-          break
-        }
-
-
-        // if (current.prev) {
-        //   // previous = current.prev
-        //   // previous.next = current.next
-        //   current.prev.next = current.next
-        // } else {
-        //   this.head = current.next
-        //   current.next.prev = null
-        // }
-        // if (current.next) {
-        //   current.next = current.prev
-        // } else {
-        //   this.tail = current.prev
-        // }
-        // res.push(current.element)
-        // this.length--        
-        // if (!all) {
-        //   break
-        // }
-
-
-
-        // if (index === 0) { // head
-        //   this.head = current.next
-        //   current.prev = null
-        // } else {
-        //   if (index != this.length - 1) { // middle
-        //     current.prev.next = current.next
-        //     current.next.prev = current.prev
-        //   } else { // tail
-        //     this.tail = current.prev
-        //     current.prev.next = null
-        //   }
-        // }
-        // res.push(current.element)
-        // if (!all) {
-        //   break
-        // }
-      }
-      // index++
-      current = current.next
-    }
-    // this.length -= res.length
-    // if (all) {
-    //   return res
-    // } else {
-    //   return res.length ? res[0] : null
-    // }
+  clear () {
+    this.items = []
   }
-  // removeElementAll () {}
-  // 连接成字符串
-  join (separate = '') {}
-  // 得到指定位置的元素
-  getEleByIndex () {}
-  // 得到头节点
-  getHead () {}
-  // 得到尾节点
-  getTail () {}
-  // 是否是空节点
-  isEmpty () {}
+  size () {
+    return this.items.length
+  }
 }
 
-
-    var r = null
-
-    r = new DoublyLinkedList()
-    r.append('first')
-    r.append('first')
-    r.append('second')
-    // // r.append('third')
-    // // r.append('fouth')
-    // // r.append('fivth')
-    r.insert(1, 'second')
-    r.insert(1, 'second')
-    r.insert(1, 'second')
-    // r.removeAt(2)
-    // r.removeElement('2.5')
-    // r.removeElement('2.5')
-    // r.removeElement('2.5')
-    r.removeElement('second', true)
-    
-    
-
-
+var r = null
+r = new Stack()
+r.push(12,34)
 
 
 
